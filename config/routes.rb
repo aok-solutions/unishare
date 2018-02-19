aedbbe081acdf0c8d7b7f79da996160e3141832c
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-02-18 21:46:49 -0600

    add custom fields to Devise user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "rides#index"
 
   resources :rides
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   get 'users/:id' => 'users#show', as: :view_profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
